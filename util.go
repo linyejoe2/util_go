9b5fe1c16537dc6abc9efa2eb8abfc23d1df9350
linyejoe2 <linyejoe2@gmail.com>
2025-02-05 11:52:44 +0800

    add new funciton CheckString() for check input is a string and not null or ''

--- util.go
+++ util.go
@@ -2,10 +2,25 @@ package util
 
 import "os"
 
+// Getenv retrieves the value of the environment variable named by the key.
+// If the variable is present in the environment and non-empty, its value is returned.
+// Otherwise, the fallback value is returned.
+//
+// Example:
+//
+//	os.Setenv("EXAMPLE_KEY", "example_value")
+//	value := Getenv("EXAMPLE_KEY", "default_value")  // Returns "example_value"
+//
+//	value = Getenv("NON_EXISTING_KEY", "default_value")  // Returns "default_value"
 func Getenv(key, fallback string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
 		return fallback
 	}
 	return value
+}
+
+func CheckString[T any](val T) bool {
+	str, ok := any(val).(string)
+	return ok && str != ""
 }
